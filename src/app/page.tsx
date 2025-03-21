b6b8e1fd39f6bc26ec879bc8138edfe9610d9113
Matt Wood <matt@mattwood.co>
2025-03-20 22:36:00 -0600

    move toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import { Header } from '@/components/common/header'
-import { Message } from '@/components/common/message'
-import { siteConfig } from '@/config/site'
+import { Header } from "@/components/common/header";
+import { Message } from "@/components/common/message";
+import { ModeToggle } from "@/components/common/mode-toggle";
+import { siteConfig } from "@/config/site";
 
 export default async () => {
   return (
     <div className="flex h-screen w-screen flex-col p-6">
       <Header />
-      <section className="flex max-w-sm animate-delay-200 animate-fade-up flex-col gap-3">
+      <section className="mx-auto flex max-w-sm flex-1 animate-delay-200 animate-fade-up flex-col gap-3">
         <p className="text-foreground/85 text-sm/5">{siteConfig.description}</p>
         <Message />
       </section>
+      <footer className="flex items-center justify-end">
+        <ModeToggle />
+      </footer>
     </div>
-  )
-}
+  );
+};
