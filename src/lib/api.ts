870709e2c3ff3ac3b5274ea786201ce8e10319df
Matt Wood <matt@mattwood.co>
2025-03-20 21:55:35 -0600

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,12 @@
-import type { ApiRoutes } from '@/server/main'
-import { magicLinkClient } from 'better-auth/client/plugins'
-import { createAuthClient } from 'better-auth/react'
-import { hc } from 'hono/client'
+import type { ApiRoutes } from "@/server/main";
+import { magicLinkClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
+import { hc } from "hono/client";
 
 // hono rpc client
-export const api = hc<ApiRoutes>('/').api
+export const api = hc<ApiRoutes>("/").api;
 
 export const { signIn, signUp, useSession, signOut } = createAuthClient({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   plugins: [magicLinkClient()],
-})
+});
